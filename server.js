2449b3648147dd95cae15282a30492a1ee07be61
slavenabd <35407666+slavenabd@users.noreply.github.com>
2024-09-14 22:56:53 +0200

    Fix server to serve files from root directory

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.use(express.static(path.join(__dirname)));
 
 // Serve index.html on the root route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-// Set the port to a fixed value or use environment variable (e.g., Heroku)
+// Set the port to a fixed value or use environment variable
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
